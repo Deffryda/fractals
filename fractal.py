0a53b286c64e5f1435019c66d1b96a4750f5d29c
Quanta <80837333+Deffryda@users.noreply.github.com>
2023-11-17 20:47:58 +0200

    Update fractal.py

--- fractal.py
+++ fractal.py
@@ -7,21 +7,23 @@ keys = []
 values  = []
 x = []
 y = []
+prop = float(input("Proportio = "))
+num_dots = int(input("Number of points = "))
 point_ = [random.randint(1,300),random.randint(0,300)]
-count_dots = 3
 
-for i in range(0,count_dots):
+
+for i in range(0,num_dots):
     keys.append(alphabet[i])
     values.append(i)
 points=dict(zip(keys, values))
 
-for i in range(0,count_dots):
+for i in range(0,num_dots):
     x.append(int(input(f"x{i} = ")))
     y.append(int(input(f"y{i} = ")))
 #print(x)
 #print(y)
 
-for i in range(0,count_dots):
+for i in range(0,num_dots):
     up()
     goto(x[i],y[i])
     down()
@@ -33,10 +35,9 @@ down()
 dot(2)
 
 while True:
-    choose = random.randint(0,count_dots-1)
+    choose = random.randint(0,num_dots-1)
     point=x[points[keys[choose]]],y[points[keys[choose]]]
-     
-    point_=[(point_[0]+point[0])/2,(point_[1]+point[1])/2] #formula
+    point_=[(point_[0]+point[0])/prop,(point_[1]+point[1])/prop] #formula
     #print(point_)
     up()
     goto(point_[0],point_[1])
